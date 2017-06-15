1b8492df848d9cf4a48d06fadefc31205d111338
Michal <bakalarskimichal@gmail.com>
2017-06-15 15:06:47 +0200

    code fixes

--- BioTester36/main.py
+++ BioTester36/main.py
@@ -4,7 +4,7 @@ from Bio.PDB import PDBParser
 
 parser = PDBParser()
 t1 = time.clock()
-structure = parser.get_structure('PHA-L', 'fa/pdb1fat.ent')
+structure = parser.get_structure('PHA-L', 'C:/data/1/pdb5lg3.ent')
 
 print(time.clock() - t1)
 print(parser.header["author"])
