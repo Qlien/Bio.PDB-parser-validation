d8f38e09fb1af94d374cddd3acafcacfaeade720
Justys <j.sasnal@gmail.com>
2017-06-15 20:57:40 +0200

    no message

--- BioTester36/main.py
+++ BioTester36/main.py
@@ -2,9 +2,16 @@ import time
 
 from Bio.PDB import PDBParser
 
-parser = PDBParser()
+#parser = PDBParser(PERMISSIVE = 1) #the same as default permissive
+# Strict parser
+strict_parser = PDBParser(PERMISSIVE=0) #restrictive
 t1 = time.clock()
-structure = parser.get_structure('PHA-L', 'fa/1fat.pdb')
+#structure = parser.get_structure('PHA-L', 'fa/1fat.pdb')
+
+strict_structure = strict_parser.get_structure('PHA-L', 'fa/1fat.pdb')
+
 
 print(time.clock() - t1)
-print(parser.header["author"])
+#print(parser.header["author"])
+
+print(strict_parser.header["name"])
